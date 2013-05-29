5784b37e7c15095dbfc4a3b364b2fdc8f76cdd66
Jeff Ching <jching@avvo.com>
2013-05-28 23:56:41 -0700

    fix filtering/faceting for pitches

--- app/controllers/pitches_controller.rb
+++ app/controllers/pitches_controller.rb
@@ -1,12 +1,31 @@
 class PitchesController < ApplicationController
+  include PitchesHelper
 
   def index
     filters = params.slice(:pitcher, :game_id, :pitch_type, :batter).tap do |hash|
       hash.each {|k,v| hash[k] = [v]}
     end
-    @pitches = Gameday::Pitch.search(page: params.fetch(:page, 1)) do
-      query { all }
-      filter :terms, filters
+    @pitches = Gameday::Pitch.search(page: params.fetch(:page, 1), per_page: 30) do
+      query do 
+        if filters.empty?
+          all
+        else
+          boolean do
+            filters.each do |k, v|
+              must { terms k, v}
+            end
+          end
+        end
+      end
+      #filter :terms, filters
+      facet 'pitcher' do
+        terms :pitcher
+      end
+      facet 'pitch_type' do
+        terms :pitch_type
+      end
     end
+
+    load_pitchers(@pitches.map(&:pitcher))
   end
 end
--- app/helpers/pitches_helper.rb
+++ app/helpers/pitches_helper.rb
@@ -1,2 +1,37 @@
 module PitchesHelper
+  PITCH_TYPE_NAMES = {
+    'ff'  =>  'Four Seam Fastball',
+    'sl'  =>  'Slider',
+    'ft'  =>  'Two Seam Fastball',
+    'ch'  =>  'Changeup',
+    'cu'  =>  'Curveball',
+    'fc'  =>  'Cutter',
+    'si'  =>  'Sinker',
+    'fs'  =>  'FS',
+    'fo'  =>  'Forkball',
+    'fa'  =>  'FA',
+  }
+  def pitch_type(type)
+    PITCH_TYPE_NAMES[type.downcase] || type
+  end
+
+  def pitcher_name(pitcher_id)
+    if pitcher = pitcher_cache[pitcher_id]
+      "#{pitcher.last}, #{pitcher.first}"
+    else
+      pitcher_id
+    end
+  end
+
+  def pitcher_cache
+    @pitcher_cache ||= {}
+  end
+
+  def load_pitchers(*pitcher_ids)
+    pp Gameday::Player.find(*pitcher_ids)
+
+
+
+    pitcher_cache.merge!(Gameday::Player.find(*pitcher_ids).index_by(&:id))
+  end
 end
